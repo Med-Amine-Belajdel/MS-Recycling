15532a9742208cd37a5a3916a34c6d9d183ebac6
AGS2023 <belajdelmedamine@gmail.com>
2023-06-10 14:54:50 -0100

    init

---
+++ assets/js/bg-animate.js
@@ -0,0 +1,28 @@
+var images = [ 'assets/js/1.webp'];
+var intervalTime = 5000;
+var currentIndex = 0;
+
+function changeBackground() {
+  currentIndex = (currentIndex + 1) % images.length;
+  var imageUrl = images[currentIndex];
+
+  // Create a new image element
+  var image = new Image();
+
+  // Set the source of the image
+  image.src = imageUrl;
+
+  // When the image is loaded, set it as the background image
+  image.onload = function() {
+    document.getElementById('hero').style.backgroundImage = 'url(' + image.src + ')';
+  };
+}
+
+setInterval(changeBackground, intervalTime);
+function openLocationInMaps() {
+  var latitude = 35.8073511788851;
+  var longitude = 10.645853935474314;
+
+  var mapsUrl = 'https://www.google.com/maps?q=' + latitude + ',' + longitude;
+  window.open(mapsUrl);
+}
